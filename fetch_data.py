f971b3300ef2aaf84fd64dd78320d2c586363832
Connor Kavleski <conkavleski@gmail.com>
2026-07-06 22:40:34 -0400

    data init

--- fetch_data.py
+++ fetch_data.py
@@ -1,9 +1,18 @@
 import duckdb
+import pandas as pd
 import requests
 
 url = "https://data.cityofnewyork.us/resource/jz4z-kudi.json"
-params = {"$limit": 50000, "$$app_token": "jQ5Aq1Bt4SKDx7YXZlATs0Iqa"}
+params = {"$limit": 3, "$$app_token": "Inxt007JrEdJQwKaX5XcYkfFC"}
 
 data = requests.get(url, params=params).json()
+df = pd.DataFrame(data)
 
-print(data)
+db = duckdb.connect("data.duckdb")
+duckdb.register(db, "data", df)
+db.execute("CREATE TABLE IF NOT EXISTS oath AS SELECT * FROM data")
+db.table("oath").show()
+db.close()
+
+# the data was able to be turned into a pandas dataframe (can be read by duck db)
+# all thats needed now is to understand how to connect to data.duckdb, write the info to it, and query (docs in firefox tab)
